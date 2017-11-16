eff568dfe6ca4f374c767f3047487d93fd3bb660
Guilherme Pompilio <guilherme.pomp@gmail.com>
2017-11-16 16:07:32 -0200

    Introducao a Windows Forms

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,39 +34,45 @@
             this.txtValor = new System.Windows.Forms.TextBox();
             this.btDeposita = new System.Windows.Forms.Button();
             this.btSaca = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // txtTitular
             // 
-            this.txtTitular.Location = new System.Drawing.Point(147, 59);
+            this.txtTitular.Location = new System.Drawing.Point(85, 30);
             this.txtTitular.Name = "txtTitular";
             this.txtTitular.Size = new System.Drawing.Size(100, 20);
             this.txtTitular.TabIndex = 0;
             // 
             // txtNumero
             // 
-            this.txtNumero.Location = new System.Drawing.Point(147, 85);
+            this.txtNumero.Location = new System.Drawing.Point(85, 56);
             this.txtNumero.Name = "txtNumero";
             this.txtNumero.Size = new System.Drawing.Size(100, 20);
             this.txtNumero.TabIndex = 1;
             // 
             // txtSaldo
             // 
-            this.txtSaldo.Location = new System.Drawing.Point(147, 111);
+            this.txtSaldo.Location = new System.Drawing.Point(85, 82);
             this.txtSaldo.Name = "txtSaldo";
             this.txtSaldo.Size = new System.Drawing.Size(100, 20);
             this.txtSaldo.TabIndex = 2;
             // 
             // txtValor
             // 
-            this.txtValor.Location = new System.Drawing.Point(86, 153);
+            this.txtValor.Location = new System.Drawing.Point(85, 108);
             this.txtValor.Name = "txtValor";
             this.txtValor.Size = new System.Drawing.Size(100, 20);
             this.txtValor.TabIndex = 3;
             // 
             // btDeposita
             // 
-            this.btDeposita.Location = new System.Drawing.Point(41, 179);
+            this.btDeposita.Location = new System.Drawing.Point(17, 149);
             this.btDeposita.Name = "btDeposita";
             this.btDeposita.Size = new System.Drawing.Size(75, 23);
             this.btDeposita.TabIndex = 4;
@@ -76,7 +82,7 @@
             // 
             // btSaca
             // 
-            this.btSaca.Location = new System.Drawing.Point(147, 179);
+            this.btSaca.Location = new System.Drawing.Point(127, 149);
             this.btSaca.Name = "btSaca";
             this.btSaca.Size = new System.Drawing.Size(75, 23);
             this.btSaca.TabIndex = 5;
@@ -84,22 +90,73 @@
             this.btSaca.UseVisualStyleBackColor = true;
             this.btSaca.Click += new System.EventHandler(this.btSaca_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Titular:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Numero:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Saldo:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(34, 111);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Valor:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btSaca);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.btDeposita);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtValor);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtSaldo);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtNumero);
+            this.groupBox1.Controls.Add(this.txtTitular);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(208, 180);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Conta";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.btSaca);
-            this.Controls.Add(this.btDeposita);
-            this.Controls.Add(this.txtValor);
-            this.Controls.Add(this.txtSaldo);
-            this.Controls.Add(this.txtNumero);
-            this.Controls.Add(this.txtTitular);
+            this.ClientSize = new System.Drawing.Size(233, 207);
+            this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -111,6 +168,11 @@
         private System.Windows.Forms.TextBox txtValor;
         private System.Windows.Forms.Button btDeposita;
         private System.Windows.Forms.Button btSaca;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
 
